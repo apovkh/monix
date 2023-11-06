7de60e0b174431bbe06de5c315afee4ee4cb8ea2
Anatolii <apovkh.dev@gmail.com>
2023-11-06 16:31:58 +0200

    feat: button component

---
+++ src/components/Layouts/LHeader.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import LBox from './LBox.vue'
+</script>
+
+<template>
+  <header class="l-header">
+    <LBox>
+      burger menu
+      logo
+    </LBox>
+  </header>
+</template>
+
+<style scoped>
+  .l-header {
+    @apply
+      flex
+      items-center
+      shadow
+      shadow-sm
+      py-2
+    ;
+  }
+</style>
