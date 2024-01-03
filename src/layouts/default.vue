5873270e8422383fd268b3062e703bbfc6cbf9f7
Anatolii Povkh <apovkh.dev@gmail.com>
2024-01-03 20:14:57 +0200

    feat: add drawers on default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,21 +1,36 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 import {
 	ABlur,
-	E_ABLUR_BACKGROUND,
+	ADrawer,
 	LFooter,
 	LHeader,
 	LMain
 } from '../components'
+const isOpenMenuDrawer = ref(false)
+const isOpenFiltersDrawer = ref(false)
+
+const onHeaderMenuClick = (): void => {
+	isOpenMenuDrawer.value = !isOpenMenuDrawer.value
+}
+
+const onFilterMenuClick = (): void => {
+	isOpenFiltersDrawer.value = !isOpenFiltersDrawer.value
+}
 </script>
 
 <template>
   <div class="t-overlay" />
   <div class="l-wrapper">
-    <LHeader />
+    <LHeader
+      @menu:click="onHeaderMenuClick"
+      @filter:click="onFilterMenuClick"
+    />
     <LMain>
       <ABlur
         radius-size="xl"
-        :background="E_ABLUR_BACKGROUND.Dark"
+        background="dark"
         class="h-full"
       >
         <slot />
@@ -24,6 +39,19 @@ import {
 
     <LFooter />
   </div>
+
+  <ADrawer
+    v-model="isOpenMenuDrawer"
+  >
+    menu
+  </ADrawer>
+
+  <ADrawer
+    v-model="isOpenFiltersDrawer"
+    location="right"
+  >
+    filters
+  </ADrawer>
 </template>
 
 <style lang="scss">
@@ -41,30 +69,29 @@ import {
       content: '';
       @apply
         absolute
-        w-[60vw]
-        h-[80vw]
-        max-h-[50vh]
-        max-w-[50vh]
+        w-[40vw]
+        h-[40vw]
+        rounded-1
         -z-1
         pointer-events-none
 
       ;
-      filter: blur(130px);
+      // filter: blur(130px);
     }
 
     &::before {
       background-color: #E7B262;
       @apply
-        top-0
-        -left-5
+        top-1
+        left-5
       ;
     }
 
     &::after {
       background-color: #88B4B3;
       @apply
-        -bottom-2
-        -right-5
+        bottom-20
+        right-5
       ;
     }
   }
