6e73a551ec6bfc9f38bea5fb6e4ec36afdb2ff7a
Anatolii Povkh <apovkh.dev@gmail.com>
2024-01-03 20:14:56 +0200

    feat: refactor ADialog

--- src/components/Atoms/ADialog/ADialog.vue
+++ src/components/Atoms/ADialog/ADialog.vue
@@ -1,32 +1,13 @@
 <script setup lang="ts">
-import { computed, reactive, ref, watch } from 'vue'
-import { v4 as uuidv4 } from 'uuid'
-import {
-	VAutocomplete,
-	VBtn,
-	VCard,
-	VDatePicker,
-	VDialog,
-	VTextField
-} from 'vuetify/components'
+import { VDialog } from 'vuetify/components'
 
-import { useDateFormat, useVModel } from '@vueuse/core'
-
-import { useCategory } from '../../../composables/useCategory'
-import { useWalletStore } from '../../../stores/wallet'
-import {
-	CATEGORY_TYPES_COSTS,
-	CATEGORY_TYPES_INCOME,
-	type IBalanceItem
-} from '../../../types/index'
+import { useVModel } from '@vueuse/core'
 
 const props = withDefaults(
 	defineProps<{
-		income?: boolean
+		modelValue: boolean
   }>(),
-	{
-		income: true
-	}
+	{}
 )
 
 defineEmits<{
@@ -34,118 +15,20 @@ defineEmits<{
 }>()
 
 const proxiedModelValue = useVModel(props, 'modelValue')
-const wallerStore = useWalletStore()
-const categoryComposable = useCategory()
-
-const categoryTypes = reactive(Object.values(CATEGORY_TYPES_COSTS))
-const selectedCategory = ref(CATEGORY_TYPES_COSTS.Food)
-
-const amount = ref<number>()
-const datePicker = ref(new Date())
-const isError = ref<boolean>(false)
-
-const dateFormat = computed(() => {
-	return useDateFormat(datePicker.value, 'YYYY-MM-DD').value
-})
-
-const actionButtonText = computed((): string => {
-	return props.income
-		? 'Додати прибуток'
-		: 'Додати витрати'
-})
-
-const titleText = computed((): string => {
-	return props.income
-		? 'Прибуток'
-		: 'Витрата'
-})
-
-const onChangeBalance = (): void => {
-	if (!amount.value || amount.value < 0) {
-		isError.value = true
-		return
-	}
-	isError.value = false
-
-	wallerStore.changeBalance({
-		income: props.income,
-		amount: props.income ? amount.value : `-${amount.value}`,
-		date: dateFormat.value,
-		type: {
-			icon: categoryComposable.value[
-				props.income
-					? CATEGORY_TYPES_INCOME.Income
-					: selectedCategory.value
-			].icon,
-			name: props.income
-				? CATEGORY_TYPES_INCOME.Income
-				: selectedCategory.value.toLocaleString()
-		},
-		id: uuidv4()
-	} as IBalanceItem)
-
-	proxiedModelValue.value = false
-}
-
-const onCancel = (): void => {
-	proxiedModelValue.value = false
-}
-
-watch(amount, (value) => {
-	if (value && value > 0) {
-		isError.value = false
-	} else {
-		isError.value = true
-	}
-})
 </script>
 
 <template>
 	<VDialog
 		v-model="proxiedModelValue"
 		width="800"
+		class="a-dialog"
   >
-    <VCard>
-      <div class="pt-5">
-				<h3 class="mx-4">{{ titleText }}</h3>
-				<div class="p-4">
-					<VAutocomplete
-						v-if="!income"
-						v-model="selectedCategory"
-						:items="categoryTypes"
-						label="Категорія"
-						class="mb-5"
-					/>
-				</div>
-
-        <VDatePicker
-          v-model="datePicker"
-          show-adjacent-months
-          width="400"
-          class="mb-5"
-        />
-
-        <div class="flex flex-wrap gap-4 p-4 justify-end">
-					<VBtn
-						:rounded="true"
-						color="gray"
-						@click="onCancel"
-					>
-						Скасувати
-					</VBtn>
-          <VBtn
-            :rounded="true"
-            color="green"
-            @click="onChangeBalance"
-          >
-            {{ actionButtonText }}
-          </VBtn>
-        </div>
-      </div>
-    </VCard>
+		<slot />
   </VDialog>
 </template>
 
 <style scoped>
-
+	.a-dialog {
+		//
+	}
 </style>
