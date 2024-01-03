289f1bb080603beefced0fae5c990a7a4da58a87
Anatolii Povkh <apovkh.dev@gmail.com>
2024-01-03 20:18:24 +0200

    chore: balance text size

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -118,7 +118,7 @@ const chartData = computed(() => {
           </VBtn>
 
           <div>
-            <b class="text-3xl mr-2">{{ wallerStore.totalBalance }}</b>
+            <b class="text-2xl mr-2">{{ wallerStore.totalBalance }}</b>
             <small class="opacity-50">UAH</small>
           </div>
 
