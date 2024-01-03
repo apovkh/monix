1bc4530b1b335b0093f5be5fa65c8fcb4cbb123d
Anatolii Povkh <apovkh.dev@gmail.com>
2024-01-03 20:11:47 +0200

    build: :construction_worker: configs

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import {
-	VBtn,
 	VIcon,
 	VTab,
 	VTabs,
@@ -9,24 +8,12 @@ import {
 	VWindowItem
 } from 'vuetify/components'
 
-import {
-	mdiMinus,
-	mdiPlus
-} from '@mdi/js'
-
 import { ADialog } from '../components'
-import { LBox } from '../components/Layouts'
 import { useWalletStore } from '../stores/wallet'
 
 const wallerStore = useWalletStore()
 const navigation = ref(null)
-const isOpenDialog = ref<boolean>(false)
 const isIncome = ref<boolean>(false)
-
-const onChangeBalanceClick = (income: boolean): void => {
-	isIncome.value = income
-	isOpenDialog.value = true
-}
 
 onMounted(async () => {
 	await wallerStore.getBalance()
@@ -35,57 +22,16 @@ onMounted(async () => {
 
 <template>
   <div class="p-main">
-    <div class="main">
-      <div class="bg-indigo py-4">
-        <LBox>
-          <h2 class="text-4xl text-center mb-4 title-1">Баланс</h2>
-          <div class="flex gap-2 items-center justify-between">
-            <VBtn
-              rounded="xl"
-              size="x-large"
-              color="green"
-              @click="onChangeBalanceClick(true)"
-            >
-              <VIcon
-                size="x-large"
-                :icon="mdiPlus"
-              />
-            </VBtn>
-
-            <div>
-              <b class="text-xl mr-2">{{ wallerStore.totalBalance }}</b>
-              <small class="opacity-50">UAH</small>
-            </div>
-
-            <VBtn
-              rounded="xl"
-              size="x-large"
-              class="px-0"
-              color="orange"
-              @click="onChangeBalanceClick(false)"
-            >
-              <VIcon
-                size="x-large"
-                :icon="mdiMinus"
-              />
-            </VBtn>
-          </div>
-        </LBox>
-      </div>
-    </div>
-
     <div class="flex-grow-1 relative">
-      <div class="absolute overflow-y-auto inset-0">
-        <VWindow v-model="navigation">
-          <VWindowItem
-            v-for="nav in wallerStore.navigations"
-            :key="nav.value"
-            :value="nav.value"
-          >
-            <Component :is="nav.component" />
-          </VWindowItem>
-        </VWindow>
-      </div>
+      <VWindow v-model="navigation">
+        <VWindowItem
+          v-for="nav in wallerStore.navigations"
+          :key="nav.value"
+          :value="nav.value"
+        >
+          <Component :is="nav.component" />
+        </VWindowItem>
+      </VWindow>
     </div>
 
     <div class="tabs shadow shadow-sm shadow-light-500">
@@ -107,7 +53,7 @@ onMounted(async () => {
   </div>
 
   <ADialog
-    v-model="isOpenDialog"
+    v-model="wallerStore.isOpenDialog"
     :income="isIncome"
   />
 </template>
